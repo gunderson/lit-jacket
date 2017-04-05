d350a6bfb31c0cc3021e79895881501e68714429
Patrick Gunderson <patrick@toolofna.com>
2017-04-05 00:29:48 -0700

    updated led pin for PCB v0.6

--- client/arduino/lit-jacket/lit-jacket.ino
+++ client/arduino/lit-jacket/lit-jacket.ino
@@ -3,15 +3,15 @@
 // ------------------------------------------------------------
 
 #include <FastLED.h>
-#define LED_PIN 13
+#define LED_PIN 15
 #define NUM_LEDS 128
 
-#define LED_TYPE    WS2812
+#define LED_TYPE WS2812
 #define COLOR_ORDER GRB
 CRGB leds[NUM_LEDS];
 
-#define BRIGHTNESS          96
-#define FRAMES_PER_SECOND  60
+#define BRIGHTNESS 96
+#define FRAMES_PER_SECOND 30
 
 int STRIP_STATE = 0;
 
@@ -29,63 +29,71 @@ int commandDataLength[] = {0, 0, NUM_LEDS * 3};
 int dataBuffer[1024]; // rgb values for each LED
 int bufferIndex = 0;
 
-boolean commandReady = false;  // whether the string is complete
+boolean commandReady = false; // whether the string is complete
 
 // ------------------------------------------------------------
 // TXRX FUNCTIONS
 // ------------------------------------------------------------
 
-void hwSerialEvent() {
-	while (HWSERIAL.available()) {
-		// get the new byte:
-		int inByte = HWSERIAL.read();
-		// Serial.println(inByte);
+void hwSerialEvent()
+{
+    while (HWSERIAL.available())
+    {
+	// get the new byte:
+	int inByte = HWSERIAL.read();
+	// Serial.println(inByte);
 
-		// activeCommand == 0 means no active command
-		if (activeCommand == 0){
+	// activeCommand == 0 means no active command
+	if (activeCommand == 0)
+	{
 
-			// reset buffer
-			resetBuffer();
+	    // reset buffer
+	    resetBuffer();
 
-			//set new command
-			activeCommand = inByte;
+	    //set new command
+	    activeCommand = inByte;
 
-			// for commands that don't expect data, the command is ready
-			if (commandDataLength[activeCommand] == 0){
-				commandReady = true;
-				break;
-			}
-			continue;
-		}
-
-		dataBuffer[bufferIndex++] = inByte;
-
-		if (bufferIndex >= commandDataLength[activeCommand]){
-			commandReady = true;
-		}
+	    // for commands that don't expect data, the command is ready
+	    if (commandDataLength[activeCommand] == 0)
+	    {
+		commandReady = true;
+		break;
+	    }
+	    continue;
 	}
+
+	dataBuffer[bufferIndex++] = inByte;
+
+	if (bufferIndex >= commandDataLength[activeCommand])
+	{
+	    commandReady = true;
+	}
+    }
 }
 
 // ------------------------------------------------------------
 
-void executeCommand(int command){
-	// Serial.print("Command: ");
-	// Serial.println(command);
-	switch (command){
-		case 1:
-			toggleLedPin();
-			break;
-		case 2:
-			writeLEDs(dataBuffer, commandDataLength[2]);
-			break;
-	}
-	resetBuffer();
+void executeCommand(int command)
+{
+    // Serial.print("Command: ");
+    // Serial.println(command);
+    switch (command)
+    {
+    case 1:
+	toggleLedPin();
+	break;
+    case 2:
+	writeLEDs(dataBuffer, commandDataLength[2]);
+	break;
+    }
+    resetBuffer();
 }
 
-void resetBuffer(){
-	activeCommand = 0;
-	bufferIndex = 0;
-	commandReady = false;
+void resetBuffer()
+{
+    activeCommand = 0;
+    bufferIndex = 0;
+    commandReady = false;
 }
 
 // ------------------------------------------------------------
@@ -116,74 +124,81 @@ float val[9];
 // Set the FreeIMU object
 FreeIMU my3IMU = FreeIMU();
 
-
-
 // ------------------------------------------------------------
 // I2C FUNCTIONS
 // ------------------------------------------------------------
-
 
 // ------------------------------------------------------------
 // SETUP
 // ------------------------------------------------------------
 
-void setup() {
-	Wire.begin();
-	delay(5);
+void setup()
+{
+    Wire.begin();
+    delay(5);
 
-	my3IMU.init(true); // the parameter enable or disable fast mode
-	delay(5);
+    my3IMU.init(true); // the parameter enable or disable fast mode
+    delay(5);
 
-	// initialize display
-	LEDS.addLeds<LED_TYPE,LED_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-	LEDS.setBrightness(BRIGHTNESS);
+    // initialize display
+    LEDS.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+    LEDS.setBrightness(BRIGHTNESS);
 
-	// initialize comm
-	HWSERIAL.begin(BAUDRATE, SERIAL_8N1);
-	// Serial.begin(115200);
-	// reset command buffer
-	commandReady = false;
-	activeCommand = 0;
-	bufferIndex = 0;
+    // initialize comm
+    HWSERIAL.begin(BAUDRATE, SERIAL_8N1);
+    // Serial.begin(115200);
+    // reset command buffer
+    commandReady = false;
+    activeCommand = 0;
+    bufferIndex = 0;
 
-	pinMode(LED_PIN, OUTPUT);
-	digitalWrite(LED_PIN, LED_STATE);
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, LED_STATE);
 }
 
 // ------------------------------------------------------------
 
-void loop() {
-	hwSerialEvent();
-	if (activeCommand > 0 && commandReady) {
-		executeCommand(activeCommand);
-	}
+void loop()
+{
+    hwSerialEvent();
+    if (activeCommand > 0 && commandReady)
+    {
+	executeCommand(activeCommand);
+    }
 }
 
 // ------------------------------------------------------------
 // COMMANDS
 // ------------------------------------------------------------
 
-void toggleLedPin(){
-	if (LED_STATE == LOW){
-		LED_STATE = HIGH;
-	} else {
-		LED_STATE = LOW;
-	}
+void toggleLedPin()
+{
+    if (LED_STATE == LOW)
+    {
+	LED_STATE = HIGH;
+    }
+    else
+    {
+	LED_STATE = LOW;
+    }
 
-	digitalWrite(LED_PIN, LED_STATE);
+    digitalWrite(LED_PIN, LED_STATE);
 }
 
 // ------------------------------------------------------------
 
-void writeLEDs(int colorBytes[], int length){
+void writeLEDs(int colorBytes[], int length)
+{
 
-	for (int i = 0; i < NUM_LEDS; i++){
-		leds[i] = CRGB( colorBytes[ i * 3 ], colorBytes[ i * 3 + 1 ], colorBytes[ i * 3 + 2 ]);
-	}
-	FastLED.show();
+    for (int i = 0; i < NUM_LEDS; i++)
+    {
+	leds[i] = CRGB(colorBytes[i * 3], colorBytes[i * 3 + 1], colorBytes[i * 3 + 2]);
+    }
+    FastLED.show();
 }
 
-void readSensors(){
-	my3IMU.getValues(val);
-	my3IMU.getYawPitchRoll(ypr);
+void readSensors()
+{
+    my3IMU.getValues(val);
+    my3IMU.getYawPitchRoll(ypr);
 }
